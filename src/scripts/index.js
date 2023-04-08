7d886e4f12994e4ad552ea6ef36ecda67f6aad16
Schnarfengargle123 <schnarfengargle123@gmail.com>
2023-04-08 10:04:57 +0100

    Adjusted responsive styles for Navigation.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,6 +18,9 @@ const navElement = document.querySelector('#navigation');
 const hamburgerMenu = document.querySelector('#hamburger-menu');
 const hamburgerOpen = document.querySelector('#hamburger-open');
 const hamburgerClose = document.querySelector('#hamburger-close');
+const navigationLinksContainer = document.querySelector('#navigation-links-container');
+const navigationLogo = document.querySelector('#navigation-logo');
+const navigationCaption = document.querySelector('#navigation-caption');
 
 hamburgerOpen.onclick = () => {
   hamburgerMenu.classList.remove('hidden');
@@ -34,6 +37,32 @@ hamburgerClose.onclick = () => {
 window.addEventListener('resize', () => {
   if (window.matchMedia('(max-width: 640px)').matches) {
     console.log("Mobile View");
+  }
+
+  if (window.matchMedia('(max-width: 940px)').matches) {
+    navigationLinksContainer.classList.add('w-72');
+    navigationLinksContainer.classList.add('mr-4');
+
+    navigationLinksContainer.classList.remove('ml-6');
+    navigationLinksContainer.classList.remove('w-96');
+
+    navigationLogo.classList.remove('sm:ml-16');
+    navigationLogo.classList.add('sm:ml-4');
+
+    navigationCaption.classList.remove('sm:mr-36');
+    navigationCaption.classList.add('sm:mr-24');
+  } else {
+    navigationLinksContainer.classList.remove('w-72');
+    navigationLinksContainer.classList.add('w-96');
+
+    navigationLinksContainer.classList.add('ml-6');
+    navigationLinksContainer.classList.add('w-96');
+
+    navigationLogo.classList.add('sm:ml-16');
+    navigationLogo.classList.remove('sm:ml-4');
+
+    navigationCaption.classList.add('sm:mr-36');
+    navigationCaption.classList.remove('sm:mr-24');
   }
 
   if (window.matchMedia('(min-width: 641px)').matches) {
